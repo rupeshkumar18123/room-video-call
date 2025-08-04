996490eebe71e35808b959c45f49b1ed9f04a9aa
Rupesh kumar <rupesh18123143@gmail.com>
2025-08-05 04:30:22 +0530

    cahnge for second try

---
+++ video-call-frontend/src/App.jsx
@@ -0,0 +1,28 @@
+// import CallInterface from './components/CallInterface';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Video Call App</h1>
+//       <CallInterface />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import React from 'react';
+import VideoCall from './components/VideoCall';
+
+function App() {
+  return (
+    <div>
+      <h1>Video Call App</h1>
+      <VideoCall />
+    </div>
+  );
+}
+
+export default App;
